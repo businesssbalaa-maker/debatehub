3cf7976fcf0d754f5cbd0aff64532e8a096bc003
Yashveer273 <yash@dekatc.com>
2026-06-04 20:59:44 +0530

    2-6-26.2

--- src/Api.js
+++ src/Api.js
@@ -4,8 +4,8 @@ import { decryptResponse } from "./utils/decrypt";
 // ==========================================
 // 📡 CONFIGURATIONS & ENVIRONMENT ANCHORS
 // ==========================================
-export const API_BASE_URL = "http://localhost:5004/";
-export const API_BASE_URL2 = "http://localhost:5004";
+export const API_BASE_URL = "https://backend.debatehub.in/";
+export const API_BASE_URL2 = "https://backend.debatehub.in";
 export const SECRET_KEY = "SECRET_KEY12356789";
 
 // ==========================================
